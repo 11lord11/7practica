79ad4867e1c08e97746edef134a0afc387e905ea
11lord11 <112806292+11lord11@users.noreply.github.com>
2022-12-12 16:39:06 +0300

    ...

---
+++ 7practica/Folder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7practica 
+{
+   
+   
+   
+   
+  
+    public class Folder
+    {
+        public static List<ReadedFile> Read(string path)
+        {
+            List<ReadedFile> files = new List<ReadedFile>();
+            if (path == null)
+            {
+                files.AddRange(GetDrives());
+            }
+            else {
+                
+                files.AddRange(GetFiles(path));
+            }
+            return files;
+        }
+        private static List<ReadedFile> GetFiles(string path)
+        {
+            List<ReadedFile> files = new List<ReadedFile>();
+            files.AddRange(ToReadedFile.ReadedFiles(Directory.GetDirectories(path).ToList()));
+            files.AddRange(ToReadedFile.ReadedFiles(Directory.GetFiles(path).ToList()));
+            return files;
+        }
+        private static List<ReadedFile> GetDrives()
+        {
+            List<ReadedFile> drives = new List<ReadedFile>();
+            foreach (DriveInfo drive in  DriveInfo.GetDrives())
+                drives.Add(new ReadedFile($"{drive.Name} Свободно {Math.Round(Convert.ToDouble(drive.AvailableFreeSpace) / 1024 / 1024 / 1024, 2)} гб", drive.Name, false));
+            return drives;
+        }
+    }
+}
---
+++ 7practica/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace _7word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string folder = null;
+            while (true)
+            {
+                List<ReadedFile> list = Folder.Read(folder);
+                int pos = Menu.Start(list,folder);
+                if (pos == -1)
+                    break;
+                else
+                    pos--;
+                if (File.Exists(list[pos].path))
+                {
+                    Process.Start(list[pos].path);
+                    break;
+                }
+                else
+                    folder = list[pos].path;
+            }
+        }
+    }
+}
